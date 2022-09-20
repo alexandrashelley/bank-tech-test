7c66f87c59192a287eec1497ee98088491cf9f88
alexandrashelley <alexbshelley@gmail.com>
2022-09-20 16:45:28 +0100

    depositing an amount returns that amount pass

--- spec/bankAccount.test.js
+++ spec/bankAccount.test.js
@@ -5,4 +5,10 @@ describe("the BankAccount object", () => {
     const bankAccount = new BankAccount();
     expect(bankAccount.getBalance()).toBe(0);
   });
+
+  it("depositing an amount of 1000 returns a value of 0", () => {
+    const bankAccount = new BankAccount();
+    bankAccount.deposit(1000);
+    expect(bankAccount.getBalance()).toBe(1000);
+  });
 });
