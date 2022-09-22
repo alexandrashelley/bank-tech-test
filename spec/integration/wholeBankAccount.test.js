23894bf98bfea8a9a099ce60528b437074886370
alexandrashelley <alexbshelley@gmail.com>
2022-09-22 11:14:33 +0100

    print method works with more than one transaction in history

--- spec/integration/wholeBankAccount.test.js
+++ spec/integration/wholeBankAccount.test.js
@@ -18,4 +18,19 @@ describe("the whole bank account", () => {
       "Wed Jan 01 2020 00:00:00 GMT+0000 (Greenwich Mean Time) || || 1000 || 1000"
     );
   });
+
+  it("calling the print method from the bank account class with two transactions in history prints formatted transactions and header", () => {
+    const bankAccount = new BankAccount();
+    bankAccount.deposit(1000);
+    bankAccount.withdraw(500);
+
+    const printer = new Printer(bankAccount.transactions);
+    printer.formatStatement();
+
+    expect(bankAccount.print()).toContain(
+      "date || credit || debit || balance\n",
+      "Wed Jan 01 2020 00:00:00 GMT+0000 (Greenwich Mean Time) || || 1000 || 1000",
+      "Wed Jan 01 2020 00:00:00 GMT+0000 (Greenwich Mean Time) || 500 || || 500"
+    );
+  });
 });
