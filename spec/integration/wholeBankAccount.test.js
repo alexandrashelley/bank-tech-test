4383711f2be50a19a5746faaec7618134c40d81f
alexandrashelley <alexbshelley@gmail.com>
2022-09-22 09:56:12 +0100

    realised last test wasn't implementing print method from main class doh

--- spec/integration/wholeBankAccount.test.js
+++ spec/integration/wholeBankAccount.test.js
@@ -8,8 +8,8 @@ describe("the whole bank account", () => {
 
   it("calling the print method from the bank account class prints one transaction from history", () => {
     const bankAccount = new BankAccount();
-    const printer = new Printer(bankAccount.transactions);
     bankAccount.deposit(1000);
+    const printer = new Printer(bankAccount.print());
     expect(printer.formatStatement()).toContain(
       "date || credit || debit || balance\n",
       "Wed Jan 01 2020 00:00:00 GMT+0000 (Greenwich Mean Time) || || 1000 || 1000",
